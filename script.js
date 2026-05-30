062113ca9fcb467d60a25129de71886e81adf1fa
azkaliano <azkaliano43@gmail.com>
2026-05-31 02:27:22 +0700

    komit 29

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ $(document).ready(function() {
     function hitungTotal() {
         let hargaPerKilo = parseInt($("#coffeeType").val());
         let jumlahKilo = parseInt($("#quantity").val());
+
+        if (isNaN(jumlahKilo) || jumlahKilo < 1) {
+            jumlahKilo = 1;
+        }
+
+        let total = hargaPerKilo * jumlahKilo;
